003293fabc26bb50d2c64e55472f473ef1037ffc
mahdikarimi422@gmail.com <mahdikarimi422@gmail.com>
2025-08-03 20:49:30 +0430

    create project

---
+++ src/main/java/ir/iraniancyber/khaneshyar/KhaneshYarApplication.java
@@ -0,0 +1,13 @@
+package ir.iraniancyber.khaneshyar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhaneshYarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhaneshYarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/iraniancyber/khaneshyar/KhaneshYarApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.iraniancyber.khaneshyar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhaneshYarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
