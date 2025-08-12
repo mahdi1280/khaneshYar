4a4fd6dd5ff21b4e840557fd8f82b6869e4374d5
parsa-a11 <atmadyparsa@gmail.com>
2025-08-12 15:22:52 +0330

    Add HintService

---
+++ src/main/java/ir/iraniancyber/khaneshyar/service/Hint/HintService.java
@@ -0,0 +1,9 @@
+package ir.iraniancyber.khaneshyar.service.Hint;
+
+import ir.iraniancyber.khaneshyar.model.Hint;
+
+public interface HintService {
+    void save(Hint hint);
+    void delete(int id);
+    void update(int id,Hint hint);
+}
